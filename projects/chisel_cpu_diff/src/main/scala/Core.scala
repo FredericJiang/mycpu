4923838cdeca0a8398d490bb54841b38c3935899
tracer-oscpu2021 <tracer@oscpu.org>
2021-12-27 16:36:38 +0800

    jsj jiangshijie 20210000    Static hostname: iscaearth          Icon name: computer-server            Chassis: server         Machine ID: 5b79f3682a3c4191a08aa2b25d1a612e            Boot ID: e382fb8501284c3492133ca9bd511cb5   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  16:36:38 up 212 days, 18:07, 26 users,  load average: 2.65, 1.87, 1.79

--- projects/chisel_cpu_diff/src/main/scala/Core.scala
+++ projects/chisel_cpu_diff/src/main/scala/Core.scala
@@ -28,7 +28,7 @@ when(!stall && !kill_stage && exe_stage_done) {if_reg_pc  := if_reg_pc + 4.U }  
 .elsewhen(kill_stage && exe_stage_done)       {if_reg_pc  := exe_pc_nxt}
 
 
-when(!stall && !kill_stage && exe_stage_done){ io.imem.inst_req   := true.B  }
+when(!stall && !kill_stage){ io.imem.inst_req   := true.B  }
 .otherwise                 { io.imem.inst_req   := false.B }
 
 io.imem.inst_addr  := if_reg_pc
