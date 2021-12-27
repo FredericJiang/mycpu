85d93e1d97528f32e8c1c8d8c6d69e3636476dbc
tracer-oscpu2021 <tracer@oscpu.org>
2021-12-27 17:14:27 +0800

    jsj jiangshijie 20210000    Static hostname: iscaearth          Icon name: computer-server            Chassis: server         Machine ID: 5b79f3682a3c4191a08aa2b25d1a612e            Boot ID: e382fb8501284c3492133ca9bd511cb5   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  17:14:27 up 212 days, 18:45, 26 users,  load average: 2.76, 1.58, 2.63

--- projects/chisel_cpu_diff/src/main/scala/Core.scala
+++ projects/chisel_cpu_diff/src/main/scala/Core.scala
@@ -24,11 +24,11 @@ val if_stage_done = WireInit(false.B)
 val exe_stage_done = WireInit(false.B)
 
 when(!stall && !kill_stage && if_stage_done) {if_reg_pc  := if_reg_pc + 4.U }  //后续可以把inst_req信号放在里面，当stall时可以不通过总线取指
-.elsewhen(stall && if_stage_done)            {if_reg_pc  := if_reg_pc}
-.elsewhen(kill_stage && if_stage_done)       {if_reg_pc  := exe_pc_nxt}
+.elsewhen(stall )        {if_reg_pc  := if_reg_pc}
+.elsewhen(kill_stage)       {if_reg_pc  := exe_pc_nxt}
 
 
-when(!stall && !kill_stage){ io.imem.inst_req   := true.B  }
+when(!stall )       { io.imem.inst_req   := true.B  }
 .otherwise          { io.imem.inst_req   := false.B }
 
 io.imem.inst_addr  := if_reg_pc
