fbc912a1daf909d9e84133b4ab625384c1aa52e8
tracer-oscpu2021 <tracer@oscpu.org>
2021-12-28 12:26:06 +0800

    jsj jiangshijie 20210000    Static hostname: iscaearth          Icon name: computer-server            Chassis: server         Machine ID: 5b79f3682a3c4191a08aa2b25d1a612e            Boot ID: e382fb8501284c3492133ca9bd511cb5   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  12:26:06 up 213 days, 13:57, 26 users,  load average: 1.99, 1.04, 1.11

--- projects/chisel_cpu_diff/src/main/scala/Core.scala
+++ projects/chisel_cpu_diff/src/main/scala/Core.scala
@@ -15,20 +15,23 @@ class Core extends Module {
 
 //********************************************************
 //Instruction Fetch Stage
-val inst_gen_ready = WireInit(false.B)
+val inst_gen_ready = WireInit(false.B)  
 val exe_stage_done = RegInit(false.B)
 
-when(inst_gen_ready || if_reg_pc === "h80000000".U)       { io.imem.inst_req   := true.B  }
-.otherwise                 { io.imem.inst_req   := false.B }
+//inst_gen_ready === exe_stage_done
+//indt_req -> reg_pc = ar.addr
+when(inst_gen_ready || if_reg_pc === "h80000000".U)      { io.imem.inst_req   := true.B  }
+.otherwise                                               { io.imem.inst_req   := false.B }
 
-when(!stall && !reg_kill_flag && exe_stage_done) { if_reg_pc := if_reg_pc + 4.U; inst_gen_ready:= true.B }  //后续可以把inst_req信号放在里面，当stall时可以不通过总线取指
+when(!stall && !reg_kill_flag && exe_stage_done) { if_reg_pc := if_reg_pc + 4.U; inst_gen_ready:= true.B } 
 .elsewhen(reg_kill_flag && exe_stage_done)       { if_reg_pc := reg_exe_pc_nxt;  reg_kill_flag := false.B; inst_gen_ready:= true.B  }
 .elsewhen(stall && exe_stage_done)               { if_reg_pc := if_reg_pc;       inst_gen_ready:= true.B  }
 
 
 
-when(!stall && !reg_kill_flag ){io.imem.inst_addr  := if_reg_pc}
-.otherwise                     {io.imem.inst_addr  := 0.U}
+//when(!stall && !reg_kill_flag ){io.imem.inst_addr  := if_reg_pc}
+//.otherwise                     {io.imem.inst_addr  := 0.U}
+io.imem.inst_addr  := if_reg_pc
 
 when(io.imem.inst_ready && reg_kill_flag )     {if_inst := 0.U}
 .otherwise                                     {if_inst := io.imem.inst_read}
