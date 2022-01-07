7f35d790cd7d00f5779ce753e5d01573e54533a3
tracer-oscpu2021 <tracer@oscpu.org>
2022-01-07 19:49:33 +0800

    jsj jiangshijie 20210000    Static hostname: iscaearth          Icon name: computer-server            Chassis: server         Machine ID: 5b79f3682a3c4191a08aa2b25d1a612e            Boot ID: e382fb8501284c3492133ca9bd511cb5   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  19:49:33 up 223 days, 21:20, 26 users,  load average: 2.49, 2.08, 2.16

--- projects/chisel_cpu_diff/vsrc/SimTop.v
+++ projects/chisel_cpu_diff/vsrc/SimTop.v
@@ -1131,19 +1131,19 @@ module Clint(
 `endif // RANDOMIZE_REG_INIT
   reg [63:0] mtime; // @[Clint.scala 31:26]
   reg [63:0] mtimecmp; // @[Clint.scala 32:26]
-  wire [63:0] _mtime_T_1 = mtime + 64'h64; // @[Clint.scala 35:18]
-  wire [63:0] _mtimecmp_T_1 = mtimecmp + io_cmp_wdata; // @[Clint.scala 38:26]
-  assign io_time_intrpt = mtime >= mtimecmp & csr_status[3] & csr_mie[7] & io_time_valid; // @[Clint.scala 46:80]
+  wire [63:0] _mtime_T_1 = mtime + 64'h1; // @[Clint.scala 34:18]
+  wire [63:0] _mtimecmp_T_1 = mtimecmp + io_cmp_wdata; // @[Clint.scala 37:26]
+  assign io_time_intrpt = mtime >= mtimecmp & csr_status[3] & csr_mie[7] & io_time_valid; // @[Clint.scala 45:80]
   always @(posedge clock) begin
     if (reset) begin // @[Clint.scala 31:26]
       mtime <= 64'h0; // @[Clint.scala 31:26]
-    end else if (io_time_valid) begin // @[Clint.scala 34:22]
-      mtime <= _mtime_T_1; // @[Clint.scala 35:9]
+    end else begin
+      mtime <= _mtime_T_1; // @[Clint.scala 34:9]
     end
     if (reset) begin // @[Clint.scala 32:26]
       mtimecmp <= 64'h0; // @[Clint.scala 32:26]
-    end else if (io_cmp_wen) begin // @[Clint.scala 37:18]
-      mtimecmp <= _mtimecmp_T_1; // @[Clint.scala 38:14]
+    end else if (io_cmp_wen) begin // @[Clint.scala 36:18]
+      mtimecmp <= _mtimecmp_T_1; // @[Clint.scala 37:14]
     end
   end
 // Register and memory initialization
