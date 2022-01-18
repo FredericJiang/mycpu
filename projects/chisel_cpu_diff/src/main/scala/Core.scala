f1fc95b09adc9fec7fe551e8ceb087b3831af43c
tracer-oscpu2021 <tracer@oscpu.org>
2022-01-18 16:58:21 +0800

    jsj jiangshijie 20210000    Static hostname: iscaearth          Icon name: computer-server            Chassis: server         Machine ID: 5b79f3682a3c4191a08aa2b25d1a612e            Boot ID: e382fb8501284c3492133ca9bd511cb5   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  16:58:21 up 234 days, 18:29, 27 users,  load average: 12.62, 10.02, 10.82

--- projects/chisel_cpu_diff/src/main/scala/Core.scala
+++ projects/chisel_cpu_diff/src/main/scala/Core.scala
@@ -188,13 +188,14 @@ val exe_op1     = Wire(UInt(64.W))
 val exe_op2     = Wire(UInt(64.W))
 
 when((exe_reg_rs1_addr === wb_reg_rd_addr && exe_reg_rs1_addr =/= mem_reg_rd_addr ) 
-&& wb_reg_rd_wen && exe_reg_op1_type === OP_REG)  {exe_op1 := wb_rd_data;  }
+&& wb_reg_rd_wen && exe_reg_op1_type === OP_REG)  {exe_op1 := wb_rd_data  }
 .otherwise                                        {exe_op1 := exe_reg_op1_data }
 
 
 when((exe_reg_rs2_addr === wb_reg_rd_addr && exe_reg_rs2_addr =/= mem_reg_rd_addr ) 
 && wb_reg_rd_wen && exe_reg_op2_type === OP_REG)  {exe_op2 := wb_rd_data}
 .otherwise                                        {exe_op2 := exe_reg_op2_data }
+
 
 when(exe_reg_alu_type === ALU_MY_INST && wb_reg_rd_addr === 10.U && wb_reg_rd_wen ){exe_reg_print := wb_rd_data}
 .elsewhen(exe_reg_alu_type === ALU_MY_INST) {exe_reg_print := exe_reg_rs1_data}
